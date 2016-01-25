991ad970889f276e247e4c0d805a06e30da8d5ab
YuxiaoHu <yuxiaohu@msn.com>
2016-01-25 15:08:49 -0800

    update installCuDNN.ps1
    (0) check input args
    (1) Correct copy from/to path
    (2) use Admin privilege to copy file

--- scripts/installCuDNN.ps1
+++ scripts/installCuDNN.ps1
@@ -1,4 +1,29 @@
+$ZipSourcePath = $args[0]
+if (-not $ZipSourcePath)
+{
+    Write-Host ('usage: installCuDNN.ps1 $downloadedZipFilePath') 
+    EXIT
+} elseif (-not (Test-Path $ZipSourcePath))
+{
+    Write-Host ('Please double check the CuDNN zip file path') 
+    EXIT
+}  
+
+$CurrentDir = $(get-location).Path
 Add-Type -A System.IO.Compression.FileSystem
-[System.IO.Compression.ZipFile]::ExtractToDirectory($args[0],'cudnn')
-cp -r -force .\cudnn\cuda\* $env:CUDA_PATH
-rm -r cudnn
+$ZipTargetDir = $CurrentDir + '\cudnn'
+[System.IO.Compression.ZipFile]::ExtractToDirectory($ZipSourcePath,$ZipTargetDir)
+
+$CopySourceDir = $ZipTargetDir+'\cuda\*'
+$CopyDestDir =  $env:CUDA_PATH
+$ScriptBlockString = "Copy-Item -Path '$CopySourceDir' -Destination '$CopyDestDir' -Recurse -Force ; rm -r $ZipTargetDir"
+$scriptblock = [scriptblock]::Create($ScriptBlockString)
+$sh = new-object -com 'Shell.Application'
+$sh.ShellExecute('powershell', "-Command $scriptblock", '', 'runas')
+
+
+
+
+
+
+
