b0ce0cfd68806b81cbb25a8a41f7cb797591b58a
Lei Zhang <leizhang@microsoft.com>
2016-02-03 08:22:52 -0800

    Add CaffeTool for feature extraction and refactor caffe.managed project

--- MSVC/caffe.managed/AssemblyInfo.cpp
+++ MSVC/caffe.managed/AssemblyInfo.cpp
@@ -11,11 +11,11 @@ using namespace System::Security::Permissions;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //
-[assembly:AssemblyTitleAttribute(L"caffeLibMC")];
+[assembly:AssemblyTitleAttribute(L"CaffeLib")];
 [assembly:AssemblyDescriptionAttribute(L"")];
 [assembly:AssemblyConfigurationAttribute(L"")];
 [assembly:AssemblyCompanyAttribute(L"")];
-[assembly:AssemblyProductAttribute(L"caffeLibMC")];
+[assembly:AssemblyProductAttribute(L"CaffeLib")];
 [assembly:AssemblyCopyrightAttribute(L"Copyright (c)  2015")];
 [assembly:AssemblyTrademarkAttribute(L"")];
 [assembly:AssemblyCultureAttribute(L"")];
