d7170278a38b3610792e8ac82ae6b5745d80851c
Lei Zhang <leizhang@microsoft.com>
2017-02-01 13:39:03 -0800

    Update python setup scripts to support calling from other modules

--- scripts/install_cudnn.py
+++ scripts/install_cudnn.py
@@ -1,18 +1,23 @@
 import argparse
 import os
+import sys
 import zipfile
 
-def parse_args():
+def parse_args(args_list):
     """Parse input arguments."""
     parser = argparse.ArgumentParser(description='install cudnn')
     parser.add_argument('zipfile', help='downloaded cudnn zip file')
-    args = parser.parse_args()
+    args = parser.parse_args(args_list)
 
     return args
 
-if __name__ == '__main__':
-    args = parse_args()
+def main(args_list):
+    args = parse_args(args_list)
 
+    print('Installing cudnn...')
     with zipfile.ZipFile(args.zipfile, 'r') as zf:
         zf.extractall('cudnn')
-
+    print('Done.')
+	
+if __name__ == '__main__':
+	main(sys.argv[1:])
