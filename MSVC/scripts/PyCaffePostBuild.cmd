8af73cbb603be133a3a81a70d64bd2436c2fff98
Lei Zhang <leizhang@microsoft.com>
2017-02-03 12:08:52 -0800

    Fix caffe build issues caused by cuda architecture and robocopy
    1. Add back multiple cuda architecture `compute_35,sm_35;compute_52,sm_52;compute_61,sm_61` to support both new Titan X
    2. Update CaffeLibPostBuild.cmd and PyCaffePostBuild.cmd to correctly handle robocopy.exe return code.

--- MSVC/scripts/PyCaffePostBuild.cmd
+++ MSVC/scripts/PyCaffePostBuild.cmd
@@ -6,3 +6,5 @@ set OUTPUT_DIR=%~3%
 
 echo PyCaffePostBuild.cmd : copy _caffe.pyd to output
 robocopy "%SOLUTION_DIR%bin\%CONFIGURATION%" "%OUTPUT_DIR%" _caffe.p* /xo /xn >nul
+if %errorlevel% geq 8 exit /b 1
+exit /b 0
