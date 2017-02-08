7a8bc700cb301535990de42228216c0ea379fdcb
Lei Zhang <leizhang@microsoft.com>
2017-02-08 15:46:45 -0800

    Fix bug in postbuild copy script
    Removed /xn in robocopy

--- MSVC/scripts/PyCaffePostBuild.cmd
+++ MSVC/scripts/PyCaffePostBuild.cmd
@@ -5,6 +5,6 @@ set CONFIGURATION=%~2%
 set OUTPUT_DIR=%~3%
 
 echo PyCaffePostBuild.cmd : copy _caffe.pyd to output
-robocopy "%SOLUTION_DIR%bin\%CONFIGURATION%" "%OUTPUT_DIR%" _caffe.p* /xo /xn >nul
+robocopy "%SOLUTION_DIR%bin\%CONFIGURATION%" "%OUTPUT_DIR%" _caffe.p* /xo >nul
 if %errorlevel% geq 8 exit /b 1
 exit /b 0
