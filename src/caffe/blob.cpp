6e2ec554958e872bd7c352023252377fe776010c
Lei Zhang <leizhang@microsoft.com>
2018-01-05 18:42:58 -0800

    Add layers needed by SSD
    Add layers from https://github.com/weiliu89/caffe/tree/ssd, and make
    necessary changes to solver.cpp, blob.cpp, io.cpp, math_functions, and
    caffe.proto.

--- src/caffe/blob.cpp
+++ src/caffe/blob.cpp
@@ -181,6 +181,7 @@ void Blob<Dtype>::ShareDiff_LE(const Blob& other) {
 // Blob<int> or Blob<unsigned int>.
 template <> void Blob<unsigned int>::Update() { NOT_IMPLEMENTED; }
 template <> void Blob<int>::Update() { NOT_IMPLEMENTED; }
+template <> void Blob<bool>::Update() { NOT_IMPLEMENTED; }
 
 template <typename Dtype>
 void Blob<Dtype>::Update() {
@@ -214,6 +215,11 @@ template <> unsigned int Blob<unsigned int>::asum_data() const {
 }
 
 template <> int Blob<int>::asum_data() const {
+  NOT_IMPLEMENTED;
+  return 0;
+}
+
+template <> bool Blob<bool>::asum_data() const {
   NOT_IMPLEMENTED;
   return 0;
 }
@@ -253,6 +259,11 @@ template <> int Blob<int>::asum_diff() const {
   return 0;
 }
 
+template <> bool Blob<bool>::asum_diff() const {
+  NOT_IMPLEMENTED;
+  return 0;
+}
+
 template <typename Dtype>
 Dtype Blob<Dtype>::asum_diff() const {
   if (!diff_) { return 0; }
@@ -284,6 +295,11 @@ template <> unsigned int Blob<unsigned int>::sumsq_data() const {
 }
 
 template <> int Blob<int>::sumsq_data() const {
+  NOT_IMPLEMENTED;
+  return 0;
+}
+
+template <> bool Blob<bool>::sumsq_data() const {
   NOT_IMPLEMENTED;
   return 0;
 }
@@ -325,6 +341,11 @@ template <> int Blob<int>::sumsq_diff() const {
   return 0;
 }
 
+template <> bool Blob<bool>::sumsq_diff() const {
+  NOT_IMPLEMENTED;
+  return 0;
+}
+
 template <typename Dtype>
 Dtype Blob<Dtype>::sumsq_diff() const {
   Dtype sumsq;
@@ -360,6 +381,10 @@ template <> void Blob<int>::scale_data(int scale_factor) {
   NOT_IMPLEMENTED;
 }
 
+template <> void Blob<bool>::scale_data(bool scale_factor) {
+  NOT_IMPLEMENTED;
+}
+
 template <typename Dtype>
 void Blob<Dtype>::scale_data(Dtype scale_factor) {
   Dtype* data;
@@ -390,6 +415,10 @@ template <> void Blob<unsigned int>::scale_diff(unsigned int scale_factor) {
 }
 
 template <> void Blob<int>::scale_diff(int scale_factor) {
+  NOT_IMPLEMENTED;
+}
+
+template <> void Blob<bool>::scale_diff(bool scale_factor) {
   NOT_IMPLEMENTED;
 }
 
@@ -575,6 +604,7 @@ void Blob<float>::ToProto(BlobProto* proto, bool write_diff) const {
 INSTANTIATE_CLASS(Blob);
 template class Blob<int>;
 template class Blob<unsigned int>;
+template class Blob<bool>;
 
 }  // namespace caffe
 
