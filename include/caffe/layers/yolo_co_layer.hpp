832435f28830188ead43b936cbd49f469a3ab8ee
ehazar <ehazar@microsoft.com>
2018-10-04 16:21:29 -0700

    YoloCoOccurrence layer

---
+++ include/caffe/layers/yolo_co_layer.hpp
@@ -0,0 +1,114 @@
+#ifndef CAFFE_YOLO_CO_LAYER_HPP_
+#define CAFFE_YOLO_CO_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+namespace caffe {
+
+/**
+* @brief Apply CoOccurrence objectness logic to negative objectness samples from RegionTarget
+* based on "PFDet: 2nd Place Solution to Open Images Challenge 2018 Object Detection Track"
+* Adapted to apply to Yolo as a weakly supervised training method: 
+*  if class Y may co-occure inside class X, avoid penalizing Y (predictions) inside X (ground truths).
+*  e.g. "Arm" may co-occure inside "Person" => 
+*       avoid penalizing a detected "Arm" inside a "Person" ground truth bounding box
+*
+* @param bottom input Blob vector
+*   -# @f$ (N \times A \times H \times W) @f$
+*      the objectness probabilities for each spatial dimension and each batch
+*   -# @f$ (N \times A \times H \times W) @f$
+*      the no-objectness probabilities (for negative samples) for each spatial dimension and each batch
+*      if objectness == no-objectness in a pixel, the loss should be zero
+*   -# @f$ (N \times G \times 1 \times 1) @f$
+*      The labels (each batch has 4 corners, and the 1 class index for each object ground truth)
+*      G = (4 + 1) x max_gt
+*   -# @f$ (N \times A \times H \times W \times 4) @f$ 
+*      generic: @f$ (N \times \prod\limits_{d=1}^{D}S_d \times 4) @f$
+*      the (x,y,w,h) bounding boxes for each batch in each of the spatial dimension
+*      @f$\prod\limits_{d=1}^{D}S_d@f$ are 1 to D arbitrary spatial dimenstions
+*       e.g. @f$ (A \times H \times W) @f$ with @f$D=3@f$
+*   -# @f$ (N \times (C + 1) \times A \times H \times W) @f$
+*      generic: @f$ (N \times (C + 1) \times \prod\limits_{d=1}^{D}S_d) @f$
+*      the predicted probabilities for each of the C classes (and objectness) for each spatial dimension and each batch
+* @param top output Blob vector
+*   -# @f$ (N \times A \times H \times W) @f$
+*      the modified no-objectness probabilities (for negative samples) for each spatial dimension and each batch
+*/
+template <typename Dtype>
+class YoloCoOccurrenceLayer : public Layer<Dtype> {
+public:
+    explicit YoloCoOccurrenceLayer(const LayerParameter& param)
+        : Layer<Dtype>(param) {
+    }
+    virtual void LayerSetUp(
+        const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top);
+    virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+                         const vector<Blob<Dtype>*>& top);
+
+    virtual inline const char* type() const {
+        return "YoloCoOccurrence";
+    }
+
+    virtual inline int ExactNumTopBlobs() const { 
+        return 1;
+    }
+
+    virtual inline int ExactNumBottomBlobs() const { 
+        return 5;
+    }
+
+protected:
+    virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+                             const vector<Blob<Dtype>*>& top);
+    virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+                             const vector<Blob<Dtype>*>& top);
+    virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+                              const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+        for (int i = 0; i < propagate_down.size(); ++i) {
+            if (propagate_down[i]) {
+                NOT_IMPLEMENTED;
+            }
+        }
+    }
+    virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+                              const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+        for (int i = 0; i < propagate_down.size(); ++i) {
+            if (propagate_down[i]) {
+                NOT_IMPLEMENTED;
+            }
+        }
+
+    }
+
+private:
+    int outer_num_;
+    int inner_num_;
+    int channels_;
+    int max_gt_;
+    map<string, int> labelmap_;
+    int* comap_class_cpu_ptr_;
+    Blob<int> comap_class_;
+    int* comap_offset_cpu_ptr_;
+    Blob<int> comap_offset_;
+    int* comap_size_cpu_ptr_;
+    Blob<int> comap_size_;
+    int* comap_cpu_ptr_;
+    Blob<int> comap_; // co-occurrence relation heap
+    float* comap_thresh_cpu_ptr_;
+    Blob<float> comap_thresh_; // class threshold
+    float* comap_obj_thresh_cpu_ptr_;
+    Blob<float> comap_obj_thresh_; // objectness threshold
+    float* comap_ixr_cpu_ptr_;
+    Blob<float> comap_ixr_; // intersection ratio
+
+    void load_labelmap(const string &filename);
+    void load_comap(const string &filename);
+};
+
+}  // namespace caffe
+
+#endif  // CAFFE_YOLO_CO_LAYER_HPP_
---
+++ src/caffe/layers/yolo_co_layer.cpp
@@ -0,0 +1,330 @@
+#include <vector>
+#include <cctype>
+
+#include <stdlib.h>
+#include <float.h>
+#include "caffe/layers/yolo_co_layer.hpp"
+#include "caffe/util/math_functions.hpp"
+
+#include "caffe/region_common.hpp"
+
+namespace {
+
+// trim from end (in place)
+static inline void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(),
+                         std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+}
+
+char *fgetl(FILE *fp) {
+    if (feof(fp))
+        return 0;
+    size_t size = 512;
+    char *line = (char *)malloc(size * sizeof(char));
+    if (!fgets(line, size, fp)) {
+        free(line);
+        return 0;
+    }
+
+    size_t curr = strlen(line);
+
+    while ((line[curr - 1] != '\n') && !feof(fp)) {
+        if (curr == size - 1) {
+            size *= 2;
+            line = (char *)realloc(line, size * sizeof(char));
+            CHECK(line) << size << " could not be allocated";
+        }
+        size_t readsize = size - curr;
+        if (readsize > INT_MAX)
+            readsize = INT_MAX - 1;
+        fgets(&line[curr], readsize, fp);
+        curr = strlen(line);
+    }
+    if (line[curr - 1] == '\n')
+        line[curr - 1] = '\0';
+
+    return line;
+}
+
+}
+
+namespace caffe {
+
+template <typename Dtype>
+void YoloCoOccurrenceLayer<Dtype>::load_labelmap(const string &filename) {
+    std::ifstream labelmap_file;
+    labelmap_file.open(filename.c_str());
+    CHECK(!labelmap_file.fail()) << "Failed to open labelmap file: " << filename << ", error: " << strerror(errno);
+
+    int id = 0;
+    labelmap_.clear();
+    while (!labelmap_file.eof()) {
+        std::string line;
+        std::getline(labelmap_file, line);
+        rtrim(line);
+        if (line.length() == 0) {
+            LOG(WARNING) << "labelmap: " << filename << " ended at line: " << id;
+            break;
+        }
+
+        labelmap_[line] = id++;
+    }
+
+    labelmap_file.close();
+}
+
+template <typename Dtype>
+void YoloCoOccurrenceLayer<Dtype>::load_comap(const string &filename) {
+    FILE *fp = fopen(filename.c_str(), "r");
+    CHECK(fp) << "Cannot open the co-occurrence map file: " << filename;
+    comap_class_cpu_ptr_ = NULL;
+    comap_offset_cpu_ptr_ = NULL;
+    comap_size_cpu_ptr_ = NULL;
+    comap_cpu_ptr_ = NULL;
+    comap_thresh_cpu_ptr_ = NULL;
+    comap_obj_thresh_cpu_ptr_ = NULL;
+    comap_ixr_cpu_ptr_ = NULL;
+
+    char part[256];
+    char whole[256];
+
+    int last_c = -1;
+    int offset = 0;
+    int cidx = 0;
+    int co_classes = 0;
+    char *line;
+    int n = 0;
+    while ((line = fgetl(fp)) != 0) {
+        float thresh = 0.6;
+        float obj_thresh = 0.2;
+        float ix = 0.9;
+        int count = sscanf(line, "%s %s %f %f %f", part, whole, &ix, &thresh, &obj_thresh);
+        CHECK_GE(count, 2) << "Few arguments reading line: " << n << " in file:" << filename;
+        CHECK_LE(count, 5) << "Error reading line: " << n << " in file:" << filename;
+
+        auto it = labelmap_.find(part);
+        CHECK(it != labelmap_.end()) << " line: " << n << " " << part << " is not in " << filename;
+        int c = it->second;
+        if (c != last_c) {
+            // Append new class
+            for (cidx = 0; cidx < co_classes; ++cidx) {
+                if (comap_class_cpu_ptr_[cidx] == c)
+                    break;
+                cidx++;
+            }
+            CHECK_EQ(cidx, co_classes) << " line: " << n << "class: " << part << " co-occurrence group discontinuity";
+            co_classes++;
+            offset = n;
+            comap_class_cpu_ptr_ = (int *)realloc(comap_class_cpu_ptr_, co_classes * sizeof(int));
+            comap_class_cpu_ptr_[cidx] = c;
+            comap_offset_cpu_ptr_ = (int *)realloc(comap_offset_cpu_ptr_, co_classes * sizeof(int));
+            comap_offset_cpu_ptr_[cidx] = offset;
+            comap_size_cpu_ptr_ = (int *)realloc(comap_size_cpu_ptr_, co_classes * sizeof(int));
+            comap_size_cpu_ptr_[cidx] = 0;
+        }
+        it = labelmap_.find(whole);
+        CHECK(it != labelmap_.end()) << " line: " << n << " " << whole << " is not in " << filename;
+        int co = it->second;
+
+        int size = comap_size_cpu_ptr_[cidx];
+        for (int coidx = 0; coidx < size; ++coidx) {
+            CHECK_NE(comap_cpu_ptr_[coidx + offset], co)
+                << " line: " << n << "class: " << part << "coclass: " << whole << " earlier line: " << coidx + offset;
+        }
+        comap_cpu_ptr_ = (int *)realloc(comap_cpu_ptr_, (n + 1) * sizeof(int));
+        comap_cpu_ptr_[n] = co;
+        comap_ixr_cpu_ptr_ = (float *)realloc(comap_ixr_cpu_ptr_, (n + 1) * sizeof(float));
+        comap_ixr_cpu_ptr_[n] = ix;
+        comap_thresh_cpu_ptr_ = (float *)realloc(comap_thresh_cpu_ptr_, (n + 1) * sizeof(float));
+        comap_thresh_cpu_ptr_[n] = thresh;
+        comap_obj_thresh_cpu_ptr_ = (float *)realloc(comap_obj_thresh_cpu_ptr_, (n + 1) * sizeof(float));
+        comap_obj_thresh_cpu_ptr_[n] = obj_thresh;
+
+        comap_size_cpu_ptr_[cidx] = size + 1;
+
+        ++n;
+    }
+    fclose(fp);
+
+    comap_class_.Reshape({ co_classes });
+    comap_class_.set_cpu_data(comap_class_cpu_ptr_);
+    comap_offset_.Reshape({ co_classes });
+    comap_offset_.set_cpu_data(comap_offset_cpu_ptr_);
+    comap_size_.Reshape({ co_classes });
+    comap_size_.set_cpu_data(comap_size_cpu_ptr_);
+
+    comap_.Reshape({ n });
+    comap_.set_cpu_data(comap_cpu_ptr_);
+    comap_thresh_.Reshape({ n });
+    comap_thresh_.set_cpu_data(comap_thresh_cpu_ptr_);
+    comap_obj_thresh_.Reshape({ n });
+    comap_obj_thresh_.set_cpu_data(comap_obj_thresh_cpu_ptr_);
+    comap_ixr_.Reshape({ n });
+    comap_ixr_.set_cpu_data(comap_ixr_cpu_ptr_);
+}
+
+template <typename Dtype>
+void YoloCoOccurrenceLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+    const YoloCoParameter& yoloco_param = this->layer_param().yoloco_param();
+    CHECK(yoloco_param.has_labelmap()) << "labelmap file is expected";
+    CHECK(yoloco_param.has_comap()) << "CoOccurrence map file is expected";
+    // Parse labelmap file and CoOccurrence map file
+    load_labelmap(yoloco_param.labelmap());
+    CHECK(!labelmap_.empty()) << "labelmap file: '" << yoloco_param.labelmap() << "' is empty";
+    load_comap(yoloco_param.comap());
+}
+
+template <typename Dtype>
+void YoloCoOccurrenceLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+    Layer<Dtype>::LayerSetUp(bottom, top);
+
+    int blob_idx = 0;
+    auto blob_obj = bottom[blob_idx++];
+    auto blob_no_obj = bottom[blob_idx++];
+    auto blob_truth = bottom[blob_idx++];
+    auto blob_bbs = bottom[blob_idx++];
+    auto blob_pred = bottom[blob_idx++];
+
+    CHECK_GE(blob_bbs->num_axes(), 3);
+    int bbs_axis = blob_bbs->num_axes() - 1;
+    CHECK_EQ(blob_bbs->shape(bbs_axis), 4);
+    outer_num_ = blob_bbs->shape(0);
+    inner_num_ = blob_bbs->count(1, bbs_axis);
+    CHECK_EQ(blob_bbs->count(), outer_num_ * inner_num_ * 4);
+
+    CHECK_EQ(blob_obj->num(), outer_num_);
+    CHECK_EQ(blob_no_obj->num(), outer_num_);
+    CHECK_EQ(blob_truth->num(), outer_num_);
+    CHECK_EQ(blob_pred->shape(0), outer_num_);
+
+    CHECK_EQ(blob_obj->count(1), inner_num_);
+    CHECK_EQ(blob_no_obj->count(1), inner_num_);
+
+    max_gt_ = blob_truth->channels() / 5;
+    CHECK_EQ(5 * max_gt_, blob_truth->channels());
+    CHECK_EQ(blob_truth->height(), 1);
+    CHECK_EQ(blob_truth->width(), 1);
+
+    CHECK_GE(blob_pred->num_axes(), 4);
+    channels_ = blob_pred->shape(1);
+    CHECK(channels_ == labelmap_.size() + 1) << 
+        "Channel count: " << channels_ << " must match label count + 1 = " << labelmap_.size() + 1;
+    CHECK_EQ(blob_pred->count(2), inner_num_);
+
+    auto target_no_obj = top[0];
+    target_no_obj->ReshapeLike(*blob_no_obj);
+}
+
+template <typename Dtype>
+void YoloCoOccurrenceLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+    int blob_idx = 0;
+    auto blob_obj = bottom[blob_idx++];
+    auto blob_no_obj = bottom[blob_idx++];
+    auto blob_truth = bottom[blob_idx++];
+    auto blob_bbs = bottom[blob_idx++];
+    auto blob_pred = bottom[blob_idx++];
+
+    auto target_no_obj = top[0];
+
+    caffe_copy(blob_no_obj->count(), blob_no_obj->cpu_data(), target_no_obj->mutable_cpu_data());
+
+    auto co_classes = comap_class_.count();
+    if (!co_classes)
+        return;
+
+    auto comap_class_data = comap_class_.cpu_data();
+    auto comap_offset_data = comap_offset_.cpu_data();
+    auto comap_size_data = comap_size_.cpu_data();
+    auto comap_data = comap_.cpu_data();
+    auto comap_thresh_data = comap_thresh_.cpu_data();
+    auto comap_obj_thresh_data = comap_obj_thresh_.cpu_data();
+    auto comap_ixr_data = comap_ixr_.cpu_data();
+    auto pred_data = blob_pred->cpu_data();
+    auto bbs_data = blob_bbs->cpu_data();
+    auto truth_data = blob_truth->cpu_data();
+    auto obj_data = blob_obj->cpu_data();
+
+    auto target_no_obj_data = target_no_obj->mutable_cpu_data();
+
+    auto classes = channels_ - 1;
+
+#pragma omp parallel for
+    for (int index = 0; index < outer_num_ * inner_num_; ++index) {
+        // index == n * inner_num_ + s
+        const int n = index / inner_num_;
+        const int s = index % inner_num_;
+        auto obj_index = n * inner_num_ + s;
+
+        // If this is a ground-truth already, nothing to do
+        if (target_no_obj_data[obj_index] > 0)
+            continue;
+
+        int bbs_index = obj_index * 4;
+        Dtype px = *(bbs_data + bbs_index + 0);
+        Dtype py = *(bbs_data + bbs_index + 1);
+        Dtype pw = *(bbs_data + bbs_index + 2);
+        Dtype ph = *(bbs_data + bbs_index + 3);
+        // Same as ground-truth logic:
+        // we explicitly ignore this zero-length bounding boxes
+        if (pw <= 0.00001 || ph <= 0.00001)
+            continue;
+
+        for (int cidx = 0; cidx < co_classes; ++cidx) {
+            auto size = comap_size_data[cidx];
+            auto offset = comap_offset_data[cidx];
+
+            for (int t = 0; t < max_gt_; ++t) {
+                auto offset_nt = n * 5 * max_gt_ + t * 5;
+                Dtype tx = *(truth_data + offset_nt + 0);
+                // If no ground-truth at this index
+                if (!tx)
+                    break;
+                Dtype ty = *(truth_data + offset_nt + 1);
+                Dtype tw = *(truth_data + offset_nt + 2);
+                Dtype th = *(truth_data + offset_nt + 3);
+                int cls = *(truth_data + offset_nt + 4); // Ground-truth class
+                // we explicitly ignore this zero-length bounding boxes
+                if (tw <= 0.00001 || th <= 0.00001)
+                    continue;
+
+                for (int i = 0; i < size; ++i) {
+                    auto co = comap_data[offset + i]; // class that c may co-occur with
+                    if (co != cls)
+                        continue;
+                    // c may co-occure with co only in one rule, so after this the loop will end
+
+                    auto obj_thresh = comap_obj_thresh_data[offset + i];
+                    auto pred_idx = n * classes * inner_num_ + classes * inner_num_ + s;
+                    auto objectness = pred_data[pred_idx];
+                    if (objectness < obj_thresh)
+                        break;
+                    auto c = comap_class_data[cidx];
+                    pred_idx = n * classes * inner_num_ + c * inner_num_ + s;
+                    auto conf = pred_data[pred_idx];
+
+                    auto thresh = comap_thresh_data[offset + i];
+                    if (conf < thresh)
+                        break;
+                    // Check intersection with co-occured class
+                    auto ixr_thresh = comap_ixr_data[offset + 1];
+                    auto ix = TBoxIntersection(px, py, pw, ph,
+                                               tx, ty, tw, th);
+                    ix /= (pw * ph); // intersection ratio
+                    if (ix >= ixr_thresh)
+                        target_no_obj_data[obj_index] = obj_data[obj_index];
+
+                    break;
+                }
+            }
+        }
+    }
+}
+
+#ifdef CPU_ONLY
+STUB_GPU(YoloCoOccurrenceLayer);
+#endif
+
+INSTANTIATE_CLASS(YoloCoOccurrenceLayer);
+REGISTER_LAYER_CLASS(YoloCoOccurrence);
+
+}  // namespace caffe
