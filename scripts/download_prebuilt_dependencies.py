5933b50ca88c3fec1617f9f9986ee6eb544d6078
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-15 18:53:02 +0000

    Adding tarfile member sanitization to extractall()

--- scripts/download_prebuilt_dependencies.py
+++ scripts/download_prebuilt_dependencies.py
@@ -75,7 +75,26 @@ def main(args_list):
     # Extract the binaries from the tar file
     with tarfile.open(dep_filename, 'r:bz2') as tar:
         print("Extracting dependencies. Please wait...")
-        tar.extractall()
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar)
         print("Done.")
     os.remove(dep_filename)
     
